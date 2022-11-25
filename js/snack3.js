643cba2b5c3106f8ad52ff45f88d6cffb7099c24
RenatoFringuello <renatofringuello@hotmail.it>
2022-11-25 16:46:26 +0100

    add part2 snack3

--- js/snack3.js
+++ js/snack3.js
@@ -1,6 +1,7 @@
 const nums = [12,234,45,65,567,76,67,34,345,35,35,2,52,64,2222,765,123,87];
-let somma = 0;
+let somma = 0, media;
 for (let i = 0; i < 10; i++) {
     somma += nums[i];
 }
-console.log(somma);
+media = somma / 10
+console.log(somma, media);
