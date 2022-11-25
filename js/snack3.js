8ed0d9851e06bf40d84e6a824963f8bbc221db7d
RenatoFringuello <renatofringuello@hotmail.it>
2022-11-25 17:08:38 +0100

    add part3 snack3

--- js/snack3.js
+++ js/snack3.js
@@ -1,7 +1,8 @@
 const nums = [12,234,45,65,567,76,67,34,345,35,35,2,52,64,2222,765,123,87];
-let sum = 0, average;
+let sum = 0, average, max = -9999999999999999;
 for (let i = 0; i < 10; i++) {
     sum += nums[i];
+    max = (max < nums[i]) ? nums[i] : max;
 }
 average = sum / 10;
-console.log(sum, average);
+console.log(sum, average, max);
