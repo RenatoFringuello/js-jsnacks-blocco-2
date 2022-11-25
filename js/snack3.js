a089f924e630aa8c34e1de06a288b23b208e3641
RenatoFringuello <renatofringuello@hotmail.it>
2022-11-25 17:56:34 +0100

    add tutor annotations

--- js/snack3.js
+++ js/snack3.js
@@ -1,9 +1,18 @@
 const nums = [12,234,45,65,567,76,67,34,345,35,35,2,52,64,2222,765,123,87];
-let sum = 0, average, max = Number.MIN_VALUE;
+
+let sum = 0, average;
+
+let max = Number.MIN_VALUE;
 // su max possiamo mettere banalmente il primo valore dell'array
+
 for (let i = 0; i < 10; i++) {
     sum += nums[i];
+    //(condizione) ? valoreDiVero : valoreDiFalso
     max = (max < nums[i]) ? nums[i] : max;
+    
+    if(max < nums[i]){
+        max = nums[i];
+    }
 }
 average = sum / 10;
 console.log(sum, average, max);
