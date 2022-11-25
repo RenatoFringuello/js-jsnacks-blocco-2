4ff5280a3d013e1fdb59ea78138f78f688417ca3
RenatoFringuello <renatofringuello@hotmail.it>
2022-11-25 17:04:21 +0100

    change variables name

--- js/snack3.js
+++ js/snack3.js
@@ -1,7 +1,7 @@
 const nums = [12,234,45,65,567,76,67,34,345,35,35,2,52,64,2222,765,123,87];
-let somma = 0, media;
+let sum = 0, average;
 for (let i = 0; i < 10; i++) {
-    somma += nums[i];
+    sum += nums[i];
 }
-media = somma / 10;
-console.log(somma, media);
+average = sum / 10;
+console.log(sum, average);
