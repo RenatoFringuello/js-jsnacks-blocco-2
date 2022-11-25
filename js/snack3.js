a5369141f251bd14b744f6396285df6fc06732dd
RenatoFringuello <renatofringuello@hotmail.it>
2022-11-25 17:34:04 +0100

    add corrections

--- js/snack3.js
+++ js/snack3.js
@@ -1,5 +1,6 @@
 const nums = [12,234,45,65,567,76,67,34,345,35,35,2,52,64,2222,765,123,87];
 let sum = 0, average, max = Number.MIN_VALUE;
+// su max possiamo mettere banalmente il primo valore dell'array
 for (let i = 0; i < 10; i++) {
     sum += nums[i];
     max = (max < nums[i]) ? nums[i] : max;
