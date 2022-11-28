93409da588f285308df1bf2709d039928fd36742
RenatoFringuello <renatofringuello@hotmail.it>
2022-11-28 12:59:33 +0100

    add corrections

--- 28:11/js/snack3.js
+++ 28:11/js/snack3.js
@@ -1,7 +1,7 @@
 const nums = [12,123,134,432,63,756,6,789,9876,434,332,1,4,5645,477];
 let sum = 0;
 for (let i = 0; i < nums.length ; i++) {
-    if(nums[i] % 2 === 1){
+    if(i % 2 === 1){
         sum += nums[i];
     }
 }
